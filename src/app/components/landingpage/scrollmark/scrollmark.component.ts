3ce16c7f096e44637d89adec194be063b70ef473
hugo <hugo.herrador@gmail.com>
2021-01-01 21:13:51 +0100

    Add logic and template to scrollmark c but not working yet

--- src/app/components/landingpage/scrollmark/scrollmark.component.ts
+++ src/app/components/landingpage/scrollmark/scrollmark.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScrollmarkComponent implements OnInit {
 
+  //public bar: any;
+  public list: HTMLCollectionOf<Element>;
+
   constructor() { }
 
+  // ngOnInit(): void {
+  //   this.bar = document.querySelectorAll(".progress");
+  //   window.addEventListener("scroll", () => {
+  //     let max = document.body.scrollHeight - innerHeight;
+  //     this.bar.style.width = `${(pageYOffset / max) * 100}%`
+  //   })
+  // }
+
   ngOnInit(): void {
+    let list = document.getElementsByClassName('progress');
+    window.addEventListener("scroll", () => {
+      let max = document.body.scrollHeight - innerHeight;
+      // Array.from(list).forEach((element) => {
+      //   element.style.width = `${(pageYOffset / max) * 100}%`;
+      // })
+      for (let item of this.list[Symbol.iterator]()) {
+        console.log(item.id);
+      }
+    })
   }
+
+  //https://stackoverflow.com/questions/22754315/for-loop-for-htmlcollection-elements
 
 }
