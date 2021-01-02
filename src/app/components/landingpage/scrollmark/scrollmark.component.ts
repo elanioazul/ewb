262053272b33f9e750bc9301c2f784cb66ea764f
hugo <hugo.herrador@gmail.com>
2021-01-02 11:43:24 +0100

    Add progress bar (scrollmark) c

--- src/app/components/landingpage/scrollmark/scrollmark.component.ts
+++ src/app/components/landingpage/scrollmark/scrollmark.component.ts
@@ -7,32 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScrollmarkComponent implements OnInit {
 
-  //public bar: any;
-  public list: HTMLCollectionOf<Element>;
+  public bar: any;
 
   constructor() { }
 
-  // ngOnInit(): void {
-  //   this.bar = document.querySelectorAll(".progress");
-  //   window.addEventListener("scroll", () => {
-  //     let max = document.body.scrollHeight - innerHeight;
-  //     this.bar.style.width = `${(pageYOffset / max) * 100}%`
-  //   })
-  // }
-
   ngOnInit(): void {
-    let list = document.getElementsByClassName('progress');
+    this.bar = document.getElementsByClassName('progress')[0];
     window.addEventListener("scroll", () => {
       let max = document.body.scrollHeight - innerHeight;
-      // Array.from(list).forEach((element) => {
-      //   element.style.width = `${(pageYOffset / max) * 100}%`;
-      // })
-      for (let item of this.list[Symbol.iterator]()) {
-        console.log(item.id);
-      }
+      this.bar.style.width = `${(pageYOffset / max) * 100}%`;
     })
   }
 
   //https://stackoverflow.com/questions/22754315/for-loop-for-htmlcollection-elements
+  //https://github.com/microsoft/TypeScript/issues/3263
 
 }
