cb4712d806fd5937c1b9b864d5595b7cfa9c065e
hugo <hugo.herrador@gmail.com>
2021-01-11 22:40:14 +0100

    Animation panning but still need to bepolish

--- src/app/components/landingpage/mapsection/mapsection.component.ts
+++ src/app/components/landingpage/mapsection/mapsection.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+import {fromLonLat} from 'ol/proj';
+import Control from 'ol/control/Control';
 @Component({
   selector: 'app-mapsection',
   templateUrl: './mapsection.component.html',
@@ -7,9 +13,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapsectionComponent implements OnInit {
 
+  public map;
+  public nl = fromLonLat([4.490559, 51.928023]);
+  public view = new View({
+    center: this.nl,
+    zoom: 6,
+  });
+  public osm = new TileLayer ({
+    visible: true,
+    opacity: 0.8,
+    source: new OSM(),
+    maxZoom: 18
+  });
+  public garzaCoord = fromLonLat([-10.654678, 6.281704]);
+
   constructor() { }
 
   ngOnInit(): void {
+    this.map = new Map ({
+      target: 'map',
+      layers: [this.osm],
+      view: this.view,
+      attribution: false
+    });
+  }
+
+  onClick() {
+    this.flyToGarza();
+  }
+
+  flyToGarza() {
+    var flyAnimation = ((done) => {
+      var duration = 2000;
+      var zoom = this.view.getZoom();
+      var parts = 2;
+      var called = false;
+      function callback(complete) {
+        --parts;
+        if (called) {
+          return;
+        }
+        if (parts === 0 || !complete) {
+          called = true;
+          done(complete);
+        }
+      }
+      this.view.animate(
+        {
+          center: this.garzaCoord,
+          duration: duration,
+        },
+        callback
+      );
+      this.view.animate(
+        {
+          zoom: zoom - 1,
+          duration: duration / 2,
+        },
+        {
+          zoom: zoom,
+          duration: duration / 2,
+        },
+        callback
+      );
+    })();
   }
 
 }
