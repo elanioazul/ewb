31feeb988e6e2ea5a167ce7b33b5215bdcdbcf37
hugo <hugo.herrador@gmail.com>
2021-02-10 20:35:37 +0100

    Add project-info-map c
    Now it needs to have in absolute position the btn for mobile

--- src/app/components/map/project-info-map/project-info-map.component.ts
+++ src/app/components/map/project-info-map/project-info-map.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+import {fromLonLat} from 'ol/proj';
 
 @Component({
   selector: 'app-project-info-map',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectInfoMapComponent implements OnInit {
 
+  public map;
+  public garzaCoord = fromLonLat([-10.654678, 6.281704]);
+  public view = new View({
+    center: this.garzaCoord,
+    zoom: 7,
+  });
+  public osm = new TileLayer ({
+    visible: true,
+    opacity: 0.8,
+    source: new OSM(),
+    maxZoom: 18
+  });
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.map = new Map ({
+      target: 'mapcanvas',
+      layers: [this.osm],
+      view: this.view,
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
+    });
+    console.log('a ver que pasa')
   }
 
 }
