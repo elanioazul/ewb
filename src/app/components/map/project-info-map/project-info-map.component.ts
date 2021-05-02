80c0015b022d34db9eaa46b7e5018633124b4f52
hugo <hugo.herrador@gmail.com>
2021-05-02 13:40:07 +0200

    Subject brings to the two subscription the same value, and I am loosing one of the values emmitted

--- src/app/components/map/project-info-map/project-info-map.component.ts
+++ src/app/components/map/project-info-map/project-info-map.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, OnDestroy } from '@angular/core';
 import { openlayersMap } from '../openlayers-map';
 import { TemplateserviceService } from 'src/app/services/templateservice.service';
 import Sidebar from "../../../js/ol5-sidebar.js";
+import LayerSwitcher from 'ol-layerswitcher';
+import {
+  RenderOptions,
+ } from 'ol-layerswitcher';
+ import Control from 'ol/control/Control';
 
 @Component({
   selector: 'app-project-info-map',
@@ -11,12 +16,16 @@ import Sidebar from "../../../js/ol5-sidebar.js";
 export class ProjectInfoMapComponent implements OnInit, AfterViewInit {
 
   mimapa: openlayersMap;
-  childTemplate?: ElementRef<HTMLElement>;
+  sidebarDiv?: ElementRef<HTMLElement>;
+  layerSwitcherDiv?: ElementRef<HTMLElement>;
 
   sidebar: Sidebar | null = null;
+  layerSwitcher: LayerSwitcher | null = null;
+  optionsToRenderLayerSwitcher: RenderOptions;
 
   constructor(private templateService: TemplateserviceService) {
-    this.templateService.template$.subscribe(t => this.childTemplate = t);
+    this.templateService.template$.subscribe(t => this.sidebarDiv = t);
+    this.templateService.template$.subscribe(t => this.layerSwitcherDiv = t);
   }
 
   ngOnInit(): void {
@@ -24,11 +33,20 @@ export class ProjectInfoMapComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.mimapa = new openlayersMap('mapcanvas');
+
     this.sidebar = new Sidebar({
-      element: this.childTemplate
+      element: this.sidebarDiv
     });
     this.sidebar.setMap(this.mimapa.map);
     this.mimapa.map.addControl(this.sidebar);
+
+    this.layerSwitcher = new LayerSwitcher({
+      reverse: true,
+      groupSelectStyle: 'group'
+    });
+    var toc = this.layerSwitcherDiv;
+    LayerSwitcher.renderPanel(this.mimapa.map, toc.nativeElement, { reverse: true})
+    this.mimapa.map.addControl(this.layerSwitcher);
 
   }
 }
--- src/app/components/map/project-info-sidebar/project-info-sidebar.component.ts
+++ src/app/components/map/project-info-sidebar/project-info-sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, TemplateRef, AfterViewInit, Inject, InjectionToken } from "@angular/core";
+import { Component, ViewChild, ElementRef, TemplateRef, AfterViewInit, Inject, InjectionToken, OnDestroy } from "@angular/core";
 import Sidebar from "../../../js/ol5-sidebar.js";
 import { TemplateserviceService } from 'src/app/services/templateservice.service';
 
@@ -7,18 +7,28 @@ import { TemplateserviceService } from 'src/app/services/templateservice.service
   templateUrl: './project-info-sidebar.component.html',
   styleUrls: ['./project-info-sidebar.component.scss'],
 })
-export class ProjectInfoSidebarComponent implements AfterViewInit {
+export class ProjectInfoSidebarComponent implements AfterViewInit, OnDestroy {
   @ViewChild("sidebar") mySidebarDiv: ElementRef<HTMLElement>;
+  @ViewChild("layers") switchLayersDiv: ElementRef<HTMLElement>;
 
   sidebar: Sidebar | null = null;
 
-  childTemplate?: any;
+  div1?: any;
+  div2?: any;
 
   constructor(private templateService: TemplateserviceService) {}
 
   ngAfterViewInit() {
     const element = this.mySidebarDiv.nativeElement;
-    this.childTemplate = element;
-    this.templateService.setTemplate(this.childTemplate)
+    this.div1 = element;
+    this.templateService.setTemplate(this.div1);
+
+    const element2 = this.switchLayersDiv.nativeElement;
+    this.div2 = element2;
+    this.templateService.setTemplate(this.div2);
+  }
+
+  ngOnDestroy() {
+    this.templateService.setTemplate();
   }
 }
