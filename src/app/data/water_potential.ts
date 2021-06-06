0316e46a74f651c6ef06cd831796cf9776cdb5c8
hugo <hugo.herrador@gmail.com>
2021-06-06 13:03:55 +0200

    Create geodata in the shape of geojson

---
+++ src/app/data/water_potential.ts
@@ -0,0 +1,105 @@
+import { GeoJSON } from 'geojson';
+
+export const waterPotentialPoints: GeoJSON = {
+  "type": "FeatureCollection",
+  "features": [
+{
+  "type": "Feature",
+  "geometry": {
+      "type": "Point",
+      "coordinates":  [ -10.65609589,6.285377042 ]
+  },
+  "properties": {
+  "id":"MEN1-7SGSY3",
+  "kind":"Well hand pump",
+  "use":"drinking, cooking and producing mineral water",
+  "householders":">50 and <500",
+  "coveredwhenfloods":"Yes"
+  }
+},
+{
+  "type": "Feature",
+  "geometry": {
+      "type": "Point",
+      "coordinates":  [ -10.64961358,6.273550428 ]
+  },
+  "properties": {
+  "id":"MEN1-7SGZF2",
+  "kind":"Well hand pump",
+  "use":"drinking, cooking and producing mineral water",
+  "householders":">50 and <500",
+  "coveredwhenfloods":"Yes"
+  }
+},
+{
+  "type": "Feature",
+  "geometry": {
+      "type": "Point",
+      "coordinates":  [ -10.6553102,6.2878955 ]
+  },
+  "properties": {
+  "id":"MENnyosie-7TMCKV",
+  "kind":"Well hand pump",
+  "use":"drinking, cooking and producing mineral water",
+  "householders":">500 ",
+  "coveredwhenfloods":"Yes"
+  }
+},
+{
+  "type": "Feature",
+  "geometry": {
+      "type": "Point",
+      "coordinates":  [ -10.65633226,6.285960255 ]
+  },
+  "properties": {
+  "id":"MEN1-7TMFPT",
+  "kind":"Well hand pump",
+  "use":"drinking, cooking and producing mineral water",
+  "householders":">500 ",
+  "coveredwhenfloods":"Yes"
+  }
+},
+{
+  "type": "Feature",
+  "geometry": {
+      "type": "Point",
+      "coordinates":  [ -10.65670148,6.284597148 ]
+  },
+  "properties": {
+  "id":"MEN1-7TMG84",
+  "kind":"Well hand pump",
+  "use":"drinking, cooking and producing mineral water",
+  "householders":">500 ",
+  "coveredwhenfloods":"Yes"
+  }
+},
+{
+  "type": "Feature",
+  "geometry": {
+      "type": "Point",
+      "coordinates":  [ -10.6583386,6.2853645 ]
+  },
+  "properties": {
+  "id":"MENnyosie-7TMLNG",
+  "kind":"Well hand pump",
+  "use":"drinking, cooking and producing mineral water",
+  "householders":">50 and <500",
+  "coveredwhenfloods":"Yes"
+  }
+},
+{
+  "type": "Feature",
+  "geometry": {
+      "type": "Point",
+      "coordinates":  [ -10.6528668,6.2767863 ]
+  },
+  "properties": {
+  "id":"MENnyosie-7VD4L9",
+  "kind":"Well hand pump",
+  "use":"drinking, cooking and producing mineral water",
+  "householders":">50 and <500",
+  "coveredwhenfloods":"Yes"
+  }
+}
+  ]
+}
