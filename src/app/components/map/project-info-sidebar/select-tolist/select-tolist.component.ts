b0f23d213ba37050c5a27a53a080e5852bde0eb7
hugo <hugo.herrador@gmail.com>
2021-06-12 14:20:03 +0200

    Add simple reactive form for dataset selection

--- src/app/components/map/project-info-sidebar/select-tolist/select-tolist.component.ts
+++ src/app/components/map/project-info-sidebar/select-tolist/select-tolist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-select-tolist',
@@ -7,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectTolistComponent implements OnInit {
 
-  constructor() { }
+  datasets: any = ['water-e', 'water-p', 'sanitation-e', 'sanitation-p']
+
+  constructor(public fb: FormBuilder) { }
+
+  myForm = this.fb.group({
+    mycontrol: ['']
+  })
+
+  onSubmit() {
+    alert(JSON.stringify(this.myForm.value))
+  }
+
+  changeDesire(e) {
+    this.myForm.get('mycontrol').setValue(e.target.value, {
+      onlySelf: true
+    })
+  }
 
   ngOnInit(): void {
   }
