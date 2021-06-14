c82db4e112355b86cc801549d64addc002473c89
hugo <hugo.herrador@gmail.com>
2021-06-14 20:55:24 +0200

    Well geojson data arrived to unsortedList c, but ngFor not working yet

--- src/app/components/map/project-info-sidebar/ul-list/ul-list.component.ts
+++ src/app/components/map/project-info-sidebar/ul-list/ul-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { WaterService } from '../../../../services/water.service';
 import { SanitationService } from '../../../../services/sanitation.service';
+import { Feature, FeatureCollection, GeoJSON as GeoJSON, Geometry, GeometryCollection, LineString, MultiLineString, MultiPoint, MultiPolygon, Point, Polygon } from 'geojson';
 
 @Component({
   selector: 'app-ul-list',
@@ -10,6 +11,8 @@ import { SanitationService } from '../../../../services/sanitation.service';
 export class UlListComponent implements OnInit, OnChanges {
 
   @Input() datasetToList: string;
+  public geoJsonToList: GeoJSON;
+  public arrayOfFeaturesToList: Array<object>;
 
   constructor(public waterS: WaterService, public sanitationS: SanitationService) { }
 
@@ -20,7 +23,28 @@ export class UlListComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges() {
-    console.log('comp para listar va a listar :' + this.datasetToList)
+    console.log('comp para listar va a listar :' + this.datasetToList);
+    this.determineWhatList();
+  }
+  determineWhatList() {
+    switch (this.datasetToList) {
+      case 'water-e':
+        this.geoJsonToList = this.waterS.getWaterExistingData;
+        break;
+      case 'water-p':
+        this.geoJsonToList = this.waterS.getWaterPotentialData;
+        break;
+      case 'sanitation-e':
+        this.geoJsonToList = this.sanitationS.getSanitationExistingData;
+        break;
+      case 'sanitation-p':
+        this.geoJsonToList = this.sanitationS.getSanitationPotentialData;
+        break;
+      default:
+        break;
+    }
+    this.arrayOfFeaturesToList = this.geoJsonToList['features'];
+    console.log('este es el geojson to list :' + JSON.stringify(this.geoJsonToList))
   }
 
 }
