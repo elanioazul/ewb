76bafdba4e78b73bd3dced508446a41c7923561d
hugo <hugo.herrador@gmail.com>
2021-07-24 20:33:06 +0200

    It tries changes: SimpleChanges to analyce input props

--- src/app/components/map/project-info-sidebar/feature-info/feature-info.component.ts
+++ src/app/components/map/project-info-sidebar/feature-info/feature-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Feature } from 'ol';
 
 @Component({
@@ -20,10 +20,21 @@ export class FeatureInfoComponent implements OnInit, OnChanges {
   ngOnInit(): void {
   }
 
-  ngOnChanges(): void {
-    console.log('tipo de data set para el switch en template :' + this.sortOfDatasetFromList)
-    console.log('el id que viene de la lista es :' + JSON.stringify(this.featureFromList))
-    console.log('la feature que viene desde el mapa :' + JSON.stringify(this.featureFromMap))
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log('tipo de data set para el switch en template :' + this.sortOfDatasetFromList)
+    // console.log('el id que viene de la lista es :' + JSON.stringify(this.featureFromList))
+    // console.log('la feature que viene desde el mapa :' + JSON.stringify(this.featureFromMap))
+    //console.log('simpleChanges obj :' + JSON.stringify(changes))
+    console.log('changes.featureFromList.currentValue' + changes.featureFromList.currentValue)
+    console.log('changes.featureFromList.previousValue' + changes.featureFromList.previousValue)
+    console.log('changes.featureFromMap.currentValue' + changes.featureFromMap.currentValue)
+    console.log('changes.featureFromMap.previousValue' + changes.featureFromMap.previousValue)
+
+
+    // if(this.featureFromMap) {
+    //   this.featureFromList = null;
+    //   this.sortOfDatasetFromList = null
+    // }
   }
 
 }
