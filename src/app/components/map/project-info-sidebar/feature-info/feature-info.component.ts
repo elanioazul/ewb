893d9ac2a85dabf47b3f1673cd4e0fbbaaf95b7e
hugo <hugo.herrador@gmail.com>
2021-07-29 20:18:00 +0200

    Success with SimpleChange detection to distinguish what input prop changed

--- src/app/components/map/project-info-sidebar/feature-info/feature-info.component.ts
+++ src/app/components/map/project-info-sidebar/feature-info/feature-info.component.ts
@@ -14,6 +14,8 @@ export class FeatureInfoComponent implements OnInit, OnChanges {
   @Input() featureFromMap: any;
   // @Input() sortOfDatasetFromMap: string;
 
+  public flipcoind: string = 'frommap' || 'fromlist';
+
   constructor() {
   }
 
@@ -21,26 +23,12 @@ export class FeatureInfoComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    // console.log('tipo de data set para el switch en template :' + this.sortOfDatasetFromList)
-    // console.log('el id que viene de la lista es :' + JSON.stringify(this.featureFromList))
-    // console.log('la feature que viene desde el mapa :' + JSON.stringify(this.featureFromMap))
-    //console.log('simpleChanges obj :' + JSON.stringify(changes))
-    // console.log('changes.featureFromList.currentValue' + changes.featureFromList.currentValue)
-    // console.log('changes.featureFromList.previousValue' + changes.featureFromList.previousValue)
-    // console.log('changes.featureFromMap.currentValue' + changes.featureFromMap.currentValue)
-    // console.log('changes.featureFromMap.previousValue' + changes.featureFromMap.previousValue)
-    for (const propName of Object.keys(changes)) {
-      const change = changes[propName];
-      const from = JSON.stringify(change.previousValue);
-      const to = JSON.stringify(change.currentValue);
-
-      console.log(propName + 'change from ' + from + ' to ' + to)
+    if (!changes.featureFromMap && changes.featureFromList) {
+      this.flipcoind = 'fromlist'
     }
-
-    // if(this.featureFromMap) {
-    //   this.featureFromList = null;
-    //   this.sortOfDatasetFromList = null
-    // }
+    if (changes.featureFromMap && !changes.featureFromList) {
+      this.flipcoind = 'frommap'
+    }
   }
 
 }
